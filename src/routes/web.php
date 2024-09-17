b3a803d8be6f17c520dd9a2302914c7a7db7bf72
tiennx <tiennx@imap.edu.vn>
2024-09-17 11:21:38 +0700

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Attributes\Developer\Controllers\AttributeController;
 use Illuminate\Support\Facades\Route;
-Route::group(['middleware' => ['auth:web', "keycloak-web-can"], 'prefix' => \Config::get('route.prefix', ''), 'as' => \Config::get('route.as', '')], function () {
+
+Route::group(['middleware' => ['auth:web', 'keycloak-web-can'], 'as' => 'hr.', 'prefix' => env('ROUTE_PREFIX')], function () use ($router) {
     Route::resource('attributes', AttributeController::class);
 });
